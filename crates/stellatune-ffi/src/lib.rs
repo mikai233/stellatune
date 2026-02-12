0eb6284dc210ac2e8616fb63cf330d627fc6cdae
mikai <dreamfever2017@yahoo.com>
2026-02-12 11:05:15 +0800

    fix: Rust format

--- crates/stellatune-ffi/src/lib.rs
+++ crates/stellatune-ffi/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod api;
-#[allow(clippy::wildcard_imports)] // flutter_rust_bridge generated module intentionally uses glob imports.
+#[allow(clippy::wildcard_imports)]
+// flutter_rust_bridge generated module intentionally uses glob imports.
 mod frb_generated;
